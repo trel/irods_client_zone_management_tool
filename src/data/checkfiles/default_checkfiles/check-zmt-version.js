55fb038aac2883da181c289d408846a076cbcae2
Terrell Russell <terrellrussell@gmail.com>
2021-11-12 21:55:50 -0500

    [17] update version check output

--- src/data/checkfiles/default_checkfiles/check-zmt-version.js
+++ src/data/checkfiles/default_checkfiles/check-zmt-version.js
@@ -10,7 +10,7 @@ export const checkZMTVersion = {
         const latestTag = await axios.get('https://api.github.com/repos/irods/irods_client_zone_management_tool/releases/latest')
         let result = {
             status: latestTag.data.tag_name === version ? 'healthy' : 'warning',
-            message: latestTag.data.tag_name === version ? `ZMT is running the latest version ${latestTag.data.tag_name}.` : `Latest ZMT version is ${latestTag.data.tag_name}, yours is ${version}`,
+            message: latestTag.data.tag_name === version ? `ZMT is running the latest released version ${latestTag.data.tag_name}.` : `This is ${version}. Latest released version is ${latestTag.data.tag_name}.`,
         }
         return result
     }
